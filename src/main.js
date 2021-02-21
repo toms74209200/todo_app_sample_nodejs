8bbcb9c86b6abca3fbff4f7bbfc3982198a745a1
toms74209200 <motomotomato@gmail.com>
2021-02-21 22:03:29 +0900

    [add]add log4js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
+const log4js = require('log4js');
 const express = require('express');
 const app = express();
 
 const index = require('./index');
 
+const log = log4js.getLogger();
+log.level = 'debug';
+
 app.use("/",index);
 
 const port = 8080;
 const server = app.listen(port, () => {
-    console.log("Server is running on port: "+ port);
+    log.info("Server is running on port: %d", port);
 });
